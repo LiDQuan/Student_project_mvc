e54c4f0730fae294d8d273d633e055756f289f37
Lyle lee <dongquanemail@gmail.com>
2019-10-30 16:53:41 +0800

    2019年10月30日16:51:32 1、配置常用目录，常用常量 2、熟悉DIRECTORY_SEPARATOR(动态分隔符)的用法，getcwd()获取当前绝对路径的用法，define定义常量的用法 3、优化后期控制器跳转 version 1.4

---
+++ MVC14-配置常用目录常量/Frame/BaseModel.class.php
@@ -0,0 +1,22 @@
+<?php 
+/*
+ *基础模型类整合，使用抽象类定义基础模型类
+ */
+// 包含数据库类
+	abstract class BaseModel{
+		// 创建私有变量保存数据库对象,当设置抽象类的时候，这个对象要设置权限为受保护的
+    	protected $db = null;
+    	// 使用构造方法，当对象初始化时自动建好
+    	public function __construct(){
+	        $arr_config = array(
+	        "db_host" => "localhost",
+	        "db_user" => "root",
+	        "db_pass" => "123456",
+	        "db_name" => "student_project",
+	        "charset" => "utf8"
+	    );
+	    $this->db = Db::getInstance($arr_config);
+		}
+	}
+
+ ?>      
---
+++ MVC14-配置常用目录常量/index.php
@@ -0,0 +1,85 @@
+<?php 
+/*
+mvc文件目录：
+----index.php 				// 一切的入口
+----Frame/					// 框架核心文件
+----App/					// 应用目录
+--------Conf/				// 配置文件目录
+--------Home/				// 前台应用目录
+------------Controller/		// 前台控制器目录
+------------Model/			// 前台模型类目录
+------------View/			// 前台视图目录
+--------Admin/				// 后台应用目录
+------------Controller/		// 后台控制器目录
+------------Model/			// 后台模型类目录
+------------View/			// 后台视图目录
+----Public/ 				// 静态资源目录(css、js、html等)
+--------Home/				// 前台资源目录
+------------Images/			// 前台图片目录
+------------Css/			// 前台CSS样式目录
+------------JS/				// 前台JS文件目录
+--------Admin/				// 后台台资源目录
+------------Images/			// 后台图片目录
+------------Css/			// 后台CSS样式目录
+------------JS/				// 后台JS文件目录
+----Uploads/				// 上传文件目录
+
+
+*/
+// 获取用户动作参数，需要访问的页面以及动作内容
+// var_dump($_GET);
+// 获取路由参数
+$p = isset($_GET['p']) ? $_GET['p'] : 'Home'; // 平台参数
+$a = isset($_GET['a']) ? $_GET['a'] : 'index'; // 接收动作名
+$c = isset($_GET['c']) ? $_GET['c'] : 'Student'; // 接收控制器名
+// 定义常量 将平台数据传入
+define("PLAT", $p);
+
+// 定义目录常量
+define("DS", DIRECTORY_SEPARATOR);                                       // 动态目录分隔符
+define("ROOT_PATH", getcwd().DS);                                        // 根目录
+define("FRAME_PATH", ROOT_PATH."Frame".DS);                              // 公共类目录
+define("MODEL_PATH", ROOT_PATH."App".DS.PLAT.DS."Model".DS);             // 模型类目录
+define("CONTROLLER_PATH", ROOT_PATH."App".DS.PLAT.DS."Controller".DS);   // 控制器目录
+define("VIEW_PATH", ROOT_PATH."App".DS.PLAT.DS."View".DS.$c.DS);         // 视图文件目录
+
+// 实现类的自动加载
+spl_autoload_register(function($className){
+    // 类文件的路径
+    $arr = array(
+        FRAME_PATH."$className.class.php",
+        MODEL_PATH."$className.class.php",
+        CONTROLLER_PATH."$className.class.php"
+    );
+
+    foreach ($arr as $filename) {
+        // 如果类文件存在，则包含
+        if(file_exists($filename)){
+            require_once "$filename";
+        }
+    }
+});
+
+
+
+// // 包含所有类文件
+
+// // 这里是所有公共类文件目录
+// require_once "./Frame/BaseController.class.php";
+// require_once "./Frame/FactroyModel.class.php";
+// require_once "./Frame/Db.class.php";
+// require_once "./Frame/BaseModel.class.php";
+// // 这里是所有控制器类文件目录
+// require_once "./App/$p/Controller/NewsController.class.php";
+// require_once "./App/$p/Controller/StudentController.class.php";
+// // 这里是所有模型类文件
+// require_once "./App/$p/Model/NewsModel.class.php";
+// require_once "./App/$p/Model/StudentModel.class.php";
+
+
+
+
+    $ControllerClassName = $c . "Controller";
+    $obj = new $ControllerClassName();
+    // 优化根据调用方法名优化if语句
+    $obj->$a();
